9871beef3f4cba703accd80796337f5b9053e34d
Kayratorvi <gavin.grooms@outlook.com>
2023-06-15 20:51:02 -0600

    Finished project website.

---
+++ header.js
@@ -0,0 +1,10 @@
+const header = `
+    <div class='author'>By Gavin Grooms</div>
+    <a style='text-decoration:none' href='./index.html'>
+        <div style='padding:5px'>
+            <h1>Hep Alien</h1>
+        </div>
+    </a>
+`;
+
+document.write(header);
---
+++ nav-bar.js
@@ -0,0 +1,28 @@
+const footer = `
+    <table>
+        <tr>
+            <td>
+                <a href='./index.html'>
+                    <div style="padding: 1% 0">Home</div>
+                </a>
+            </td>
+            <td>
+                <a href='./about.html'>
+                    <div style="padding: 1% 0">About</div>
+                </a>
+            </td>
+            <td>
+                <a href='./contact.html'>
+                    <div style="padding: 1% 0">Contact Us</div>
+                </a>
+            </td>
+            <td>
+                <a href='./set_list.html'>
+                    <div style="padding: 1% 0">Set List</div>
+                </a>
+            </td>
+        </tr>
+    </table>
+    `;
+
+document.write(footer);
